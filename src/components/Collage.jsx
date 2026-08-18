953f055644bb8154a82b0c5a603b0a4ca719345d
Alicia Buelow <alicia@buelow.com.au>
2026-08-18 13:31:24 +0800

    Create basic Collage grid and add images

--- src/components/Collage.jsx
+++ src/components/Collage.jsx
@@ -1,12 +1,19 @@
 import Item from "./Item";
 import items from "../data/items";
+import '../styles/Collage.css';
 
 export default function Collage() {
     return (
         <div className="Collage">
-            {items.map((item, i) => {
-                return <Item key={i} image={item.image} category={item.category} alt={item.alt} />;
-            })}
+            <div className="grid-container">
+                {items.map((item, i) => {
+                    return (
+                        <div className="grid-item" key={i}>
+                            <Item image={item.image} category={item.category} alt={item.alt} />
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     )
 }
