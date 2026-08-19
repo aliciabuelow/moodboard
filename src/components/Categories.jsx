3ad67a50ae3c17f639b05726d4f36bff453aa8d6
Alicia Buelow <alicia@buelow.com.au>
2026-08-19 18:17:00 +0800

    Style and place h1, style h2

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,44 +1,46 @@
+import '../styles/Categories.css';
+
 export default function Categories({ setCategory }) {
     return (
         <div className="Categories">
             <button 
-                className="category-btn"
+                className="category-all"
                 onClick={() => setCategory('all')}
             >
                 All
             </button>
             <button 
-                className="category-btn"
+                className="category-design"
                 onClick={() => setCategory('design')}
             >
                 Design
             </button>
             <button 
-                className="category-btn"
+                className="category-home"
                 onClick={() => setCategory('home')}
             >
                 Home
             </button>
             <button 
-                className="category-btn"
+                className="category-art"
                 onClick={() => setCategory('art')}
             >
                 Art
             </button>
             <button 
-                className="category-btn"
+                className="category-dev"
                 onClick={() => setCategory('development')}
             >
                 Development
             </button>
             <button 
-                className="category-btn"
+                className="category-photo"
                 onClick={() => setCategory('photography')}
             >
                 Photography
             </button>
             <button 
-                className="category-btn"
+                className="category-fashion"
                 onClick={() => setCategory('fashion')}
             >
                 Fashion
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,13 @@ export default function Hero() {
     return (
         <div className="Hero">
             <h1 className="title">
-                Moodboard
+                <div className="letter-one">M</div>
+                <div className="letter-two">O</div>
+                <div className="letter-three">O</div>
+                <div className="letter-four">D</div>
             </h1>
+        
+            <h2 className="subtitle">A curated collection of digital artifacts, representive of my personal taste, style, and interests.</h2>
         </div>
     )
 }
