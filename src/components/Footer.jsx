884283c84ed3814e25d6442a6ee94215afd2351e
Alicia Buelow <alicia@buelow.com.au>
2026-08-21 17:17:32 +0800

    Create basic animated footer marquee

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import '../styles/Footer.css';
+
 export default function Footer() {
     return (
         <div className="Footer">
-            <footer>Footer</footer>
+            <footer>
+                <div className="footer-text">
+                    <span>PERSONAL MOOD BOARD VOL. 1</span>
+                    <span>EXPERIMENTAL UI 2026</span>
+                    <span>ALICIA BUELOW</span>
+                    <span>ALICIABUELOW.DEV</span>
+                    <span>FRONTEND DEVELOPER</span>
+                </div>
+            </footer>
         </div>
     )
 }
